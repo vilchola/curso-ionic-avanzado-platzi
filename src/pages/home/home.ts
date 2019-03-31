d488c2a33c936d725d09353626318595c8a2e8f6
Henry Vilchez <henry.vilchez@acorde.com.ve>
2019-03-31 06:42:35 -0400

    Nueva interface: User

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ConversationPage } from '../conversation/conversation';
 import { LoginPage } from '../login/login';
+import { User } from '../../interfaces/user';
 
 @Component({
   selector: 'page-home',
@@ -10,7 +11,18 @@ import { LoginPage } from '../login/login';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    let myUser: User = {
+      nick: 'Henry Vilchez',
+      subnick: '@vilchola',
+      age: 32,
+      email: 'roberto1286@gmail.com',
+      friend: false,
+      uid: 1
+    };
+    let users: User[] = [
+      myUser
+    ];
+    console.log(myUser);
   }
 
   goToConversation() {
