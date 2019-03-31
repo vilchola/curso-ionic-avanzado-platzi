8b8fe9d52c28e2134aeab0b05fd433b6975cde41
Henry Vilchez <henry.vilchez@acorde.com.ve>
2019-03-31 07:36:25 -0400

    NgIf aplicado en la lista de usuarios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,19 +16,19 @@ export class HomePage {
     let usuario1: User = {
       nick: 'Henry Vilchez',
       email: 'roberto1286@gmail.com',
-      friend: false,
+      friend: true,
       uid: 1
     };
     let usuario2: User = {
       nick: 'Alejandro Vilchez',
       email: 'ale181085@gmail.com',
-      friend: false,
+      friend: true,
       uid: 2
     };
     let usuario3: User = {
       nick: 'Sebastian Vilchez',
       email: 'hsvbronco@hotmail.com',
-      friend: false,
+      friend: true,
       uid: 3
     };
     let usuario4: User = {
